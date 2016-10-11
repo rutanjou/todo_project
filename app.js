f2f442b534fdb9cbc7e08a1b85472995b1703a4a
Ru Tan <jutan2012@gmail.com>
2016-10-11 15:34:24 +0200

    Ajout function de changer la couleur

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 $("button").click(function(){
 
-   var recoveredInput = $("input").val();    
-   $(".faiteVotrelisteici").append("<li>"+recoveredInput+"</li>"); 
-   ; console.log("recoveredInput"); }
-}
+   var recoveredInput = $("input").val(); 
+
+	$(".faiteVotrelisteici").append("<li>"+recoveredInput+"</li>");
+	console.log(recoveredInput);    
+   
+   $("ul").toggleClass("Fait");
+   console.log(recoveredInput);
+});
 	
+
